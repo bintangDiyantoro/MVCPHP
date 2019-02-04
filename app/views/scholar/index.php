a01567d29809a4e7a999759540099121270d2bef
Bintang Diyantoro <bintangdiyantoro@gmail.com>
2019-02-04 14:41:20 +0700

    Flash Message

--- app/views/scholar/index.php
+++ app/views/scholar/index.php
@@ -1,5 +1,11 @@
 <div class="row">
-    <div class="col-6">
+    <div class="col-lg-6">
+        <?php Flasher::flash(); ?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-6">
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
             Add Scholar
         </button><br><br>
